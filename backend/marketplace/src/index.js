7a7d11370f62748a30c89d7ae99f4de7ce48c15a
tonorz <praneshrajavel@yahoo.com>
2026-08-27 22:18:06 +0530

    Basic stuff

--- backend/marketplace/src/index.js
+++ backend/marketplace/src/index.js
@@ -1,1 +1,12 @@
-//Mar
+import express from "express";
+
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.send("Test");
+});
+
+app.listen(PORT, () => {
+  console.log(`APP STARTED ON ${PORT}`)
+});
